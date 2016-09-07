f7fb3c8a83201d450bc4d3b9cdc6fc46929b4221
Dano Oravec <danoravec@gmail.com>
2016-09-07 23:13:38 +0200

    Square-shaped character is moves randomly by 1 field when chessboard clicked

--- chess-game.py
+++ chess-game.py
@@ -5,26 +5,27 @@ except ImportError:
     from Tkinter import *
 
 
+root = Tk()
+canvas = Canvas(root, width=256, height=256)
+canvas.pack()
+
 class Game:
 
     def __init__(self):
-        self.root = Tk()
         self.clicks = 0
-        self.canvas = Canvas(self.root, width=256, height=256)
-        self.canvas.pack()
 
     def draw_chessboard(self, color):
         x1 = 0
         y1 = 0
         x2 = 32
-        self.canvas.create_rectangle(0, 0, 256, 256, fill=color)
+        canvas.create_rectangle(0, 0, 256, 256, fill=color)
 
         for i in range(8):
             for j in range(4):
                 if color == "black":
-                    self.canvas.create_rectangle(x1, y1, x2, y1 + 32, fill="white")
+                    canvas.create_rectangle(x1, y1, x2, y1 + 32, fill="white")
                 else:
-                    self.canvas.create_rectangle(x1, y1, x2, y1 + 32, fill="black")
+                    canvas.create_rectangle(x1, y1, x2, y1 + 32, fill="black")
                 x1 += 64
                 if x2 + 64 <= 256:
                     x2 += 64
@@ -49,43 +50,48 @@ class Game:
 
 class Hero:
 
-    def __init__(self, pos_x, pos_y, field_width, board_width):
+    def __init__(self, field_width, board_width):
         self.x = board_width / 2
-        self.y = pos_y / 2
+        self.y = board_width / 2
         self.field_side = field_width
         self.board_side = board_width
+        self.act_hero = None
 
     def move_random(self):
-        print(self.x, self.board_side, self.field_side)
         direction = random.randint(0, 4)
-        print(direction)
         # Movement right
         if direction == 0 and self.x + self.field_side <= self.board_side:
-            print("in")
+            print("0")
             self.x += self.field_side
+            canvas.delete(self.act_hero)
+            self.act_hero = None
         # Movement left
         elif direction == 1 and self.x - self.field_side > 0:
-            print("in")
-
+            print("1")
             self.x -= self.field_side
+            canvas.delete(self.act_hero)
+            self.act_hero = None
         # Movement up
         elif direction == 2 and self.y - self.field_side > 0:
-            print("in")
+            print("2")
             self.y -= self.field_side
+            canvas.delete(self.act_hero)
+            self.act_hero = None
         # Movement down
         elif direction == 3 and self.y + self.field_side <= self.board_side:
-            print("in")
+            print("3")
             self.y += self.field_side
-        print(self.x)
+            canvas.delete(self.act_hero)
+            self.act_hero = None
 
-
-def draw_hero(pos):
-    print("drawing")
-    hero.move_random()
-    chessboard.canvas.create_rectangle(hero.x, hero.y, 32, 32, fill='red')
+    def draw_hero(self, pos):
+        print("drawing")
+        hero.move_random()
+        if self.act_hero is None:
+            self.act_hero = canvas.create_rectangle(hero.x, hero.y, hero.x + 32, hero.y + 32, fill='red')
 
 chessboard = Game()
-hero = Hero(0, 0, 32, 256)
+hero = Hero(32, 256)
 chessboard.draw_chessboard("black")
-chessboard.canvas.bind("<Button-1>", draw_hero)
-chessboard.root.mainloop()
+canvas.bind("<Button-1>", hero.draw_hero)
+root.mainloop()
